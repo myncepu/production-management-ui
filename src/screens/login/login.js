670df8488c4402c8ad41cf6b432d3b45ba12be47
Yan Pengqiang <yan2010@live.com>
2018-07-19 15:43:11 +0800

    update login.js

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -6,40 +6,174 @@
  */
 import React, { Component } from 'react'
 import {
+  TextInput,
+  TouchableOpacity,
   SafeAreaView,
   View,
+  Text,
+  StyleSheet,
 } from 'react-native'
 import {
   Button,
-  Item,
-  Label,
-  Input,
-  Form,
-  Text
 } from 'native-base'
+import colors from '../../theme/variables/commonColor'
+import Icon from 'react-native-vector-icons/Ionicons'
+import { CheckBox } from 'react-native-elements'
 
-class FloatingLabel extends Component {
+class Login extends Component {
+  static navigationOptions = {
+    headerTitle: (
+      <Text style={{ fontSize: 20, color: 'white' }} >
+        登陆
+      </Text>
+    ),
+    headerRight: (
+      <TouchableOpacity style={{ marginRight: 10, padding: 5, marginBottom: 10 }}>
+        <Icon size={30} name="ios-more" style={{ color: 'white' }} />
+      </TouchableOpacity>
+    ),
+    headerStyle: {
+      backgroundColor: colors.brandPrimary,
+    },
+  }
+
   render() {
     return (
-      <SafeAreaView>
-        <View>
-          <Form>
-            <Item floatingLabel>
-              <Label>Username</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel last>
-              <Label>Password</Label>
-              <Input secureTextEntry />
-            </Item>
-          </Form>
-          <Button block style={{ margin: 15, marginTop: 50 }}>
-            <Text>Sign In</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ margin: 15 }}>
+
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.commonTitle}>登陆</Text>
+          </View>
+
+          <View style={styles.form}>
+
+            <View style={styles.formItem}>
+              <View style={styles.formSpanWrapper}>
+                <Text style={styles.formSpan}>用户名:</Text>
+              </View>
+              <View style={styles.formInputWrapper}>
+                <TextInput
+                  style={styles.formInput}
+                  placeholder='请输入用户名'
+                  fontSize={20}
+                />
+              </View>
+            </View>
+
+            <View style={styles.formItem}>
+              <View style={styles.formSpanWrapper}>
+                <Text style={styles.formSpan}>密码:</Text>
+              </View>
+              <View style={styles.formInputWrapper}>
+                <TextInput
+                  style={styles.formInput}
+                  placeholder='请输入密码'
+                  fontSize={20}
+                />
+              </View>
+            </View>
+
+          </View>
+
+          <View style={styles.licence}>
+            <CheckBox
+              left
+              size={15}
+              containerStyle={styles.checkbox}
+              textStyle={{ width: 0 }}
+              checkedIcon='check-square-o'
+              uncheckedIcon='square-o'
+              checkedColor={colors.brandPrimary}
+              checked={true}
+              onPress={() => null}
+            />
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text style={{ color: '#555' }}>已阅读并同意</Text>
+              <TouchableOpacity>
+                <Text
+                  style={{ color: colors.brandPrimary }}
+                  onPress={() => {
+                    // TODO: navigate到协议页
+                  }}
+                >
+                  《相关条款》
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <Button block style={{ marginTop: 10 }}>
+            <Text style={{ color: 'white', fontSize: 20 }}>登陆</Text>
           </Button>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 20 }}>
+            <TouchableOpacity>
+              <Text style={{ color: colors.brandPrimary, fontWeight: 'bold' }}>无法登陆?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={{ color: colors.brandPrimary, fontWeight: 'bold' }}>新用户注册</Text>
+            </TouchableOpacity>
+          </View>
         </View>
+
+        <View style={styles.footer}>
+          <Text style={{ fontSize: 15, color: '#aaa' }}>Copyright {'\u00A9'} 2018 国网太原供电公司 变电检修室.</Text>
+        </View>
+
       </SafeAreaView>
     )
   }
 }
 
-export default FloatingLabel
+const styles = StyleSheet.create({
+  commonTitle: {
+    fontSize: 25,
+    color: '#555',
+  },
+  form: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+  formItem: {
+    flexDirection: 'row',
+    height: 50,
+    borderBottomColor: '#eee',
+    borderBottomWidth: 0.5,
+    alignItems: 'center',
+  },
+  formSpanWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  formSpan: {
+    fontSize: 20,
+    color: '#555',
+  },
+  formInputWrapper: {
+    flex: 3,
+    justifyContent: 'center',
+  },
+  formInput: {
+    flex: 1,
+    paddingHorizontal: 5,
+  },
+  licence: {
+    paddingTop: 20,
+    flexDirection: 'row',
+  },
+  checkbox: {
+    marginHorizontal: 0,
+    paddingHorizontal: 0,
+    backgroundColor: 'transparent',
+    width: 20,
+  },
+  footer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+})
+export default Login
