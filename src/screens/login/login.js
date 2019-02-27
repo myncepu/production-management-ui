51bc511528ac250f7e80aa6c0b2e551dc88e975c
Yan Pengqiang <yan2010@live.com>
2019-02-27 22:12:51 +0800

    add before commit

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -21,6 +21,14 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import { CheckBox } from 'react-native-elements'
 
 class Login extends Component {
+  state = {
+    phoneNumber: '',
+  }
+
+  setPhoneNumber = (phoneNumber) => {
+    this.setState({ phoneNumber })
+  }
+
   static navigationOptions = {
     headerTitle: (
       <Text style={{ fontSize: 20, color: 'white' }} >
@@ -50,13 +58,16 @@ class Login extends Component {
 
             <View style={styles.formItem}>
               <View style={styles.formSpanWrapper}>
-                <Text style={styles.formSpan}>用户名:</Text>
+                <Text style={styles.formSpan}>手机号码:</Text>
               </View>
               <View style={styles.formInputWrapper}>
                 <TextInput
+                  maxLength={13}
                   style={styles.formInput}
-                  placeholder='请输入用户名'
+                  placeholder='请输入手机号码'
                   fontSize={20}
+                  onChangeText={(text) => this.setPhoneNumber(text)}
+                  value={this.state.phoneNumber}
                 />
               </View>
             </View>
@@ -117,7 +128,7 @@ class Login extends Component {
         </View>
 
         <View style={styles.footer}>
-          <Text style={{ fontSize: 15, color: '#aaa' }}>Copyright {'\u00A9'} 2018 国网太原供电公司 变电检修室.</Text>
+          <Text style={{ fontSize: 15, color: '#aaa' }}>{'\u00A9'} 2018 国网太原供电公司 变电检修室</Text>
         </View>
 
       </SafeAreaView>
@@ -131,6 +142,8 @@ const styles = StyleSheet.create({
     color: '#555',
   },
   form: {
+    borderColor: '#888',
+    borderWidth: 1,
     marginTop: 20,
     backgroundColor: 'white',
     borderRadius: 5,
@@ -138,21 +151,22 @@ const styles = StyleSheet.create({
   formItem: {
     flexDirection: 'row',
     height: 50,
-    borderBottomColor: '#eee',
-    borderBottomWidth: 0.5,
+    borderBottomColor: '#999',
+    borderBottomWidth: 1,
     alignItems: 'center',
   },
   formSpanWrapper: {
-    flex: 1,
-    alignItems: 'center',
+    flex: 2,
+    alignItems: 'flex-start',
     justifyContent: 'center',
+    paddingLeft: 10,
   },
   formSpan: {
     fontSize: 20,
     color: '#555',
   },
   formInputWrapper: {
-    flex: 3,
+    flex: 5,
     justifyContent: 'center',
   },
   formInput: {
